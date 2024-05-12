4b4832606650859ae2b148de7e0ff1b561d9136b
tvhfrks12678 <tvhfrks12678@gmail.com>
2024-05-12 12:14:32 +0900

    Global Pending UI - Synchronizing URLs to Form State

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction } from '@remix-run/node';
+import type { LinksFunction, LoaderFunctionArgs } from '@remix-run/node';
 import {
   Form,
   json,
@@ -11,10 +11,12 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from '@remix-run/react';
 
 import appStylesHref from './app.css?url';
 import { createEmptyContact, getContacts } from './data';
+import { useEffect } from 'react';
 
 export const action = async () => {
   const contact = await createEmptyContact();
@@ -25,13 +27,23 @@ export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: appStylesHref },
 ];
 
-export const loader = async () => {
-  const contacts = await getContacts();
-  return json({ contacts });
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get('q');
+  const contacts = await getContacts(q);
+  return json({ contacts, q });
 };
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const searchField = document.getElementById('q');
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || '';
+    }
+  }, [q]);
   return (
     <html lang="en">
       <head>
@@ -48,6 +60,7 @@ export default function App() {
               <input
                 id="q"
                 aria-label="Search contacts"
+                defaultValue={q || ''}
                 placeholder="Search"
                 type="search"
                 name="q"
@@ -87,7 +100,9 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div
+          className={navigation.state === 'loading' ? 'loading' : ''}
+          id="detail">
           <Outlet />
         </div>
 
